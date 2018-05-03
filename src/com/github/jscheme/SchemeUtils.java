3a9a463f73990c820a8627b0a3cec7a77ef16280
shell <shell>
2018-05-03 20:08:21 +0800

    remove

--- src/com/github/jscheme/SchemeUtils.java
+++ src/com/github/jscheme/SchemeUtils.java
@@ -34,43 +34,7 @@ public class SchemeUtils {
         return first(rest(x));
     }
 
-    static void stringify(Object x, StringBuffer buf) {
-        if (x == null)
-            buf.append("()");
-        else if (x instanceof Double) {
-            double d = ((Double) x).doubleValue();
-            if (Math.round(d) == d) buf.append((long) d);
-            else buf.append(d);
-        } else if (x instanceof Character) {
-            buf.append(x);
-        } else if (x instanceof Pair) {
-            ((Pair) x).stringifyPair(buf);
-        } else if (x instanceof char[]) {
-            char[] chars = (char[]) x;
-            for (int i = 0; i < chars.length; i++) {
-                buf.append(chars[i]);
-            }
-        } else if (x instanceof Object[]) {
-            Object[] v = (Object[]) x;
-            buf.append("#(");
-            for (int i = 0; i < v.length; i++) {
-                stringify(v[i],  buf);
-                if (i != v.length - 1) buf.append(' ');
-            }
-            buf.append(')');
-        } else if (x == TRUE) {
-            buf.append("#t");
-        } else if (x == FALSE) {
-            buf.append("#f");
-        } else {
-            buf.append(x);
-        }
-    }
 
-    static String stringify(Object x) {
-        StringBuffer buf = new StringBuffer();
-        stringify(x, buf);
-        return buf.toString();
-    }
+
 
 }
